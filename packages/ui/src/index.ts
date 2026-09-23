0acfbbc23f35e553c84f0c9eacf5daa7b4f39c30
Samuel Ocampo Echeverry <131569500+SamOkampo@users.noreply.github.com>
2026-09-23 12:22:20 -0500

    bootstrap: add packages/ui/src/index.ts

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,12 @@
+export * from "./motion.js";
+export * from "./materials.js";
+
+export const typographyTokens = Object.freeze({
+  moneyNumericVariant: "tabular-nums",
+  maxReadingWidthCh: 68,
+});
+
+export const touchTokens = Object.freeze({
+  minimumTargetPx: 44,
+  capsuleHeightPx: 48,
+});
